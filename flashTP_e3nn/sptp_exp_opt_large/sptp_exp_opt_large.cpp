b0ec2172bc1375184cfa5aee3cc747ca9899f1a1
sylee0124 <syulee0124@gmail.com>
2025-07-28 18:05:42 +0900

    Change in compilation

--- flashTP_e3nn/sptp_exp_opt_large/sptp_exp_opt_large.cpp
+++ flashTP_e3nn/sptp_exp_opt_large/sptp_exp_opt_large.cpp
@@ -392,10 +392,10 @@ void sptp_linear_bwd_bwd_v2_shared_exp(
 }
 
 
-#ifndef FLASHTP_TORCHSCRIPT
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("sptp_linear_fwd_v2_shared_exp", &sptp_linear_fwd_v2_shared_exp, "sptp_linear_fwd_v2_shared_exp");
-    m.def("sptp_linear_bwd_v1_shared_exp", &sptp_linear_bwd_v1_shared_exp, "sptp_linear_bwd_v1_shared_exp");
-    m.def("sptp_linear_bwd_bwd_v2_shared_exp", &sptp_linear_bwd_bwd_v2_shared_exp, "sptp_linear_bwd_bwd_v2_shared_exp");
-}
-#endif
+// #ifndef FLASHTP_TORCHSCRIPT
+// PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+//     m.def("sptp_linear_fwd_v2_shared_exp", &sptp_linear_fwd_v2_shared_exp, "sptp_linear_fwd_v2_shared_exp");
+//     m.def("sptp_linear_bwd_v1_shared_exp", &sptp_linear_bwd_v1_shared_exp, "sptp_linear_bwd_v1_shared_exp");
+//     m.def("sptp_linear_bwd_bwd_v2_shared_exp", &sptp_linear_bwd_bwd_v2_shared_exp, "sptp_linear_bwd_bwd_v2_shared_exp");
+// }
+// #endif
